8bbaf895e421fa1a7e3671c2bc5c47deba5df7a5
Nguyendinhtan101 <bigskull116@gmail.com>
2025-02-21 10:05:06 +0700

    bai thuc hanh

---
+++ DemoMVC/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models.Entities
+{
+    public class Employee : Person
+    {
+    public string EmployeeID { get; set; }
+    public string Company { get; set; }
+    }
+}
---
+++ DemoMVC/Models/Entities/Person.cs
@@ -0,0 +1,13 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models.Entities
+{
+    public class Person
+    {
+        [Key]
+        public String PersonID { get; set;}
+        public String HoTen { get;set;}
+        public String QueQuan { get; set;}
+    }
+}
