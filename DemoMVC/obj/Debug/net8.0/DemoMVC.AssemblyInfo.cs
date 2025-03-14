a95ad98d2568db973d68163e33d18c7c6b4b1238
Nguyendinhtan101 <bigskull116@gmail.com>
2025-03-14 10:37:10 +0700

    battap

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bbaf895e421fa1a7e3671c2bc5c47deba5df7a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e897cd427bcec2e698ba52fa122a2a8d190562")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
